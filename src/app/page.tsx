425d9526692edf482445c3fbe49d8d36e4576b46
Gyanendra SIngh <g.singghhh@gmail.com>
2025-06-23 23:43:21 +0530

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function Home(){
+export default function Home() {
   return (
-    <>
-    <h1>Home Page</h1>
-    <Link href={"/blog"}>Blogs</Link>
-    <Link href={"/products"} >Products</Link>
-    <Link href={"/articles/breaking-news?lang=en"}>Read in English</Link>
-    <Link href={"/articles/breaking-news?lang=fr"}>Read in French</Link>
-    <Link href={"/order-product"}>Order Product</Link>
-    <Link href={"/dashboard"}>Dashboard</Link>
-    </>
-  ) 
+    <div>
+      <h1>Home Page</h1>
+      <div className="flex gap-3">
+        <Link href={"/blog"}>Blogs</Link>
+        <Link href={"/products"}>Products</Link>
+        <Link href={"/articles/breaking-news?lang=en"}>Read in English</Link>
+        <Link href={"/articles/breaking-news?lang=fr"}>Read in French</Link>
+        <Link href={"/order-product"}>Order Product</Link>
+        <Link href={"/dashboard"}>Dashboard</Link>
+      </div>
+    </div>
+  );
 }
